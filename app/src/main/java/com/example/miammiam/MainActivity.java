24ff3ed47a9db09d4e471fe0f951424029d753f7
Beatriz <beatrizponces.oficial@gmail.com>
2024-11-25 23:14:39 -0400

    Agora tá rodando..

--- app/src/main/java/com/example/miammiam/MainActivity.java
+++ app/src/main/java/com/example/miammiam/MainActivity.java
@@ -1,16 +1,9 @@
 package com.example.miammiam;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageButton;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import com.example.miammiam.user.CadastroActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -18,40 +11,12 @@ import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageButton btnUser;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setupToolbar();
-        setupUserButton();
         setupBottomNavigation();
-    }
-
-    /**
-     * Configura a Toolbar personalizada.
-     */
-    private void setupToolbar() {
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        // Oculta o título da Toolbar (se necessário)
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayShowTitleEnabled(false);
-        }
-    }
-
-    /**
-     * Configura o botão para redirecionar à tela de Cadastro de Usuário.
-     */
-    private void setupUserButton() {
-        btnUser = findViewById(R.id.btnUsuario);
-        btnUser.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, CadastroActivity.class);
-            startActivity(intent);
-        });
     }
 
     /**
